1abefdd49cf8c58a40b2f10f6e5ed8290440b16b
KariukiAbel <abelmuriithi@gmail.com>
2022-04-17 20:59:01 +0300

    All works fine as I have tested it on postman

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
+from api import serializers
+from api.models import Task
+from api.serializers import TaskSerializer
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
@@ -16,3 +19,9 @@ def apiOverview(request):
         'Delete':'/task-delete/<str:pk>',
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
