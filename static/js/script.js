8ba04d18ccbcce2700ba6533f226e2f3a6a5d70c
KariukiAbel <abelmuriithi@gmail.com>
2022-04-18 15:26:56 +0300

    Created a csrftoken in js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,24 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break
+            }
+        }
+    }
+    return cookieValue
+}
+
+var csrftoken = getCookie('csrftoken');
 buildList();
 
 function buildList() {
     var wrapper = document.getElementById('list-wrapper')
+    wrapper.innerHTML = '';
     var url = 'http://localhost:8000/api/task-list/'
     fetch(url)
         .then((resp) => resp.json())
@@ -24,3 +41,22 @@ function buildList() {
             }
         })
 }
+
+var form_wrapper = document.getElementById('form-wrapper')
+form_wrapper.addEventListener('submit', function(e) {
+    // prevent the form from submitting on its own
+    e.preventDefault()
+    var url = 'http://localhost:8000/api/task-create/'
+    var title = document.getElementById('title').value;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            'title': title
+        })
+    }).then(function(response) {
+        buildList()
+    })
+})
